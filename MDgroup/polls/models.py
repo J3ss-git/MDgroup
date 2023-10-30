d41ed20b7034b799f234beeacd2c6edd9d9ab97e
j3ss-git <elize.leroux76@gmail.com>
2023-10-30 03:47:30 -0700

    Added a docmentation string to the class choice in models.py

--- MDgroup/polls/models.py
+++ MDgroup/polls/models.py
@@ -15,6 +15,8 @@ class Question(models.Model):
 
  # Database of choices   
 class Choice(models.Model):
+    """This class makes a database called choice:
+    """
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
