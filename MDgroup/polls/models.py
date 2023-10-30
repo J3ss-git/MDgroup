91ddd9d166abe9e80f879e78354861c8fcb384d9
j3ss-git <elize.leroux76@gmail.com>
2023-10-30 03:44:51 -0700

    Added a docmentation string to the method of the class question in models.py

--- MDgroup/polls/models.py
+++ MDgroup/polls/models.py
@@ -9,6 +9,8 @@ class Question(models.Model):
     pub_date = models.DateTimeField('date published')
     
     def __str__(self):
+        """This method returns the text of the question:
+        """
         return self.question_text
 
  # Database of choices   
